794d98f479ccbb089f52daf1c699a4f870c2ebbd
Abil Adriansyah <2300018022@webmail.uad.ac.id>
2026-05-13 12:48:30 +0700

    Menambahkan 5 route Laravel

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,34 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+/*
+|--------------------------------------------------------------------------
+| Route String Biasa
+|--------------------------------------------------------------------------
+*/
 
 Route::get('/hello', function () {
     return 'Hello, World!';
+});
+
+Route::get('/profil', function () {
+    return 'Ini adalah halaman profil';
+});
+
+Route::get('/kontak', function () {
+    return 'Halaman kontak website';
+});
+
+/*
+|--------------------------------------------------------------------------
+| Route Dengan Parameter
+|--------------------------------------------------------------------------
+*/
+
+Route::get('/user/{nama}', function ($nama) {
+    return 'Selamat datang, ' . $nama;
+});
+
+Route::get('/matkul/{nama}/{kelas}', function ($nama, $kelas) {
+    return 'Mata kuliah: ' . $nama . ', Kelas: ' . $kelas;
 });
